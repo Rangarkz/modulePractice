a1cef68e4e55cf7148ef7e2a6905bf0833629657
750584 <srr66166@gmail.com>
2025-02-15 16:11:11 +0530

    add initial elements, views, and collections modules with routing and components

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ElementsRoutingModule,
+    ElementsHomeComponent
+  ], 
+  exports: [ ElementsHomeComponent ]
+})
+export class ElementsModule { }
